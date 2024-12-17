b30a368f8a30f7e060cbf4d0aa184ac12d659649
pkritt <krittapat37819@gmail.com>
2024-12-17 15:46:30 +0700

    finish

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt");
+	string text;
+	dest << "-------------------- BOOM ---------------------\n";
+
+	while (getline(source,text)){
+	dest << text<<endl;
+	}
+	dest << "-------------------- HA!! ---------------------\n";
+    source.close();
+    dest.close();
+	return 0;
+}
 
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <cmath>
 
+using namespace std;
+
+int main(){
+    ifstream source;
+    string num;
+    double sum=0;
+    double summ=0;
+    int i=0;
+	source.open("score.txt");
+	while (getline(source,num))
+	{
+	    sum=sum + stod(num);
+	    summ=summ+(stod(num)*stod(num));
+	    i++;
+	    
+	}
+	
+    cout << "Number of data = "<<i<<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<<sum/i<<endl;
+    
+    
+    cout << "Standard deviation = "<<sqrt((summ/i)-((sum/i)*(sum/i)));
+}
